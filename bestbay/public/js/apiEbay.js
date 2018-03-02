dcc621fa0d35932110d8f942b5646e5c484bd93c
Daniel Afonso <Daniel-Af@outlook.com>
2018-03-02 18:11:17 +0000

    Filtrado por:
    -Precios Maximos
    -Precios Mínimos
    -Total de resultados
    -Artículos por pagina

--- bestbay/public/js/apiEbay.js
+++ bestbay/public/js/apiEbay.js
@@ -6,12 +6,12 @@ function searchEbay(b) {
     searchArtsEbay(b);
 }
 
-function searchArtsEbay(b) {
+function searchArtsEbay() {
     classToggle("#divEbay .prelo", "none");
     $.ajax({
         jsonp: "callback",
         dataType: "jsonp",
-        url: createUrlE(b),
+        url: createUrlE(),
         success: function (response) {
             //console.log(response);
             createObjsE(response);
@@ -22,7 +22,7 @@ function searchArtsEbay(b) {
             console.log("ApiEbay completado");
             //console.log(artsEbay);
             rellenarEbay();
-            paginar("#eResults .card", "#pagination-1", 10);
+            paginar("#eResults .card", "#pagination-1", objectFilter.pageResults);
         },
         error: function (error, codigo, algo) {
             console.error(error);
@@ -32,7 +32,7 @@ function searchArtsEbay(b) {
     });
 }
 
-function createUrlE(b) {
+function createUrlE() {
 
     var url = "http://svcs.ebay.com/services/search/FindingService/v1";
     url += "?OPERATION-NAME=findItemsAdvanced";
@@ -41,13 +41,16 @@ function createUrlE(b) {
     url += "&GLOBAL-ID=EBAY-ES";
     url += "&RESPONSE-DATA-FORMAT=JSON";
     url += "&REST-PAYLOAD";
-    url += "&paginationInput.entriesPerPage=" + objectFilter.size;
+    url += "&paginationInput.entriesPerPage=" + objectFilter.totalResults;
     url += "&sortOrder=" + objectFilter.ordEbay;
-    url += "&keywords=" + b;
+    url += "&keywords=" + objectFilter.keyword;
     url += "&categoryId=" + objectFilter.catEbaySet;
     url += "&descriptionSearch=true";
+    url += "&itemFilter(0).name=MaxPrice";
+    url += "&itemFilter(0).value=" + objectFilter.maxPrice;
+    url += "&itemFilter(1).name=MinPrice";
+    url += "&itemFilter(1).value=" + objectFilter.minPrice;
     //url += "&sortOrder=asc";
-
 
     /*
         var url = "http://svcs.ebay.com/services/search/FindingService/v1";
--- bestbay/public/js/objs/objs.js
+++ bestbay/public/js/objs/objs.js
@@ -24,9 +24,13 @@ function artBestbuy() {
 function optFiltrado() {
     /* Smartphones, TV y Health, Fitness and Beauty */
     this.maxPriceD = 9999;
+    this.minPriceD = 300;
+    this.totalResultsD = 100;
+    this.pageResultsD = 10;
+    this.keyword = "";
     this.catEbay = ['15032', '293', '26395'];
     this.catBestbuy = ['pcmcat209400050001', 'abcat0101000', 'pcmcat242800050021'];
-    this.ordEbay = ['PricePlusShippingHighest', 'PricePlusShippingLowest'];
+    this.ordEbay = ['PricePlusShippingLowest', 'PricePlusShippingHighest'];
     this.ordBestbuy = ['asc', 'desc'];
 
     this.catEbaySet = this.catEbay[0];
@@ -34,9 +38,15 @@ function optFiltrado() {
     this.ordEbaySet = this.ordEbay[0];
     this.ordBestbuySet = this.ordBestbuy[0];
 
-    this.maxPrice = this.maxPriceD;   
+    this.maxPrice = this.maxPriceD;
+    this.minPrice = this.minPriceD;
     this.brand = "";
-    this.size = 100;
+    this.totalResults = this.totalResultsD;
+    this.pageResults = this.pageResultsD;
+}
+
+optFiltrado.prototype.setKeyword = function (e) {
+    this.keyword = e;
 }
 
 optFiltrado.prototype.selCats = function (e) {
@@ -50,11 +60,19 @@ optFiltrado.prototype.setOrder = function (e) {
 }
 
 optFiltrado.prototype.setMaxPrice = function (e) {
-    this.maxPrice = e;
+    this.maxPrice = e > 0 & e > this.minPrice ? e : this.maxPriceD;
 }
 
-optFiltrado.prototype.setSize = function (e) {
-    this.size = e;
+optFiltrado.prototype.setMinPrice = function (e) {
+    this.minPrice = e > 0 & e < this.maxPrice ? e : this.minPriceD;
+}
+
+optFiltrado.prototype.setTotalResults = function (e) {
+    this.totalResults = e < 100 & e > 1 ? e : this.totalResultsD;
+}
+
+optFiltrado.prototype.setPageResults = function (e) {
+    this.pageResults = e > 1 ? e : this.pageResultsD;
 }
 
 optFiltrado.prototype.setBrands = function (e) {
